728982f6ab941d3bbd9f50df04ae67e41aa23e78
yoooun45 <yoooun45@naver.com>
2018-08-25 20:12:47 +0900

    connect firebase

---
+++ app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/GetInform.java
@@ -0,0 +1,14 @@
+package leesimjeonsim.user.cosmeticlifecycle;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class GetInform extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_inform);
+    }
+
+}
--- app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/MainActivity.java
+++ app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/MainActivity.java
@@ -1,13 +1,105 @@
 package leesimjeonsim.user.cosmeticlifecycle;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static Activity _MainActivity;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLoginButton;
+    private Button mJoinButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        _MainActivity = MainActivity.this;
+        mEmailField = findViewById(R.id.input_email);
+        mPasswordField = findViewById(R.id.input_pw);
+        mLoginButton = findViewById(R.id.login_button);
+        mJoinButton = findViewById(R.id.join_button);
+
+        mLoginButton.setOnClickListener(this);
+        mJoinButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // add Checking if user is signed in (non-null) and update UI accordingly.
+
+        //mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        finish();
+        if (mAuthListener != null) {
+            //  mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d("TAG", "createAccount:" + email);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        Log.d("TAG", "onClick success");
+        if (i == R.id.join_button) {
+            Log.d("TAG", "createID button success");
+            createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+            Intent intent = new Intent(getBaseContext(),GetInform.class);
+            startActivity(intent);
+
+        }
+        else if (i == R.id.login_button){
+
+        }
+    }
+
 }
