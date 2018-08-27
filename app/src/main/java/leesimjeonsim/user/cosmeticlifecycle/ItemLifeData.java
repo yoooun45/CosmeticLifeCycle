19feda363dc4c3cb61796fd4016bc0a44954a6fe
Jeonyujeong <dbwjd8752@naver.com>
2018-08-27 23:27:41 +0900

    ItemLifeData 수정, LifeItem 클래스 없애버림

--- app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/ItemLifeData.java
+++ app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/ItemLifeData.java
@@ -5,48 +5,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ItemLifeData {
-    public static final List<LifeItem> ITEMS = new ArrayList<LifeItem>();
+class ItemLifeData {
+    public String id;
+    public String title;
+    public String content;
+    public String details;
+    public String d_day;
 
-    public static final Map<String, LifeItem> ITEM_MAP = new HashMap<String, LifeItem>();
+    public ItemLifeData() {}
 
-    private static final int COUNT = 25;
-
-    static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
-        }
+    public ItemLifeData(String id,String title, String content, String details, String d_day) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.details = details;
+        this.d_day = d_day;
     }
 
-    private static void addItem(LifeItem item) {
-        ITEMS.add(item);
-        ITEM_MAP.put(item.id, item);
-    }
-
-
-    private static LifeItem createDummyItem(int position) {
-        return new LifeItem(String.valueOf(position), "노세범 " + position, "content","details","30");
-    }
-
-    public static class LifeItem {
-        public final String id;
-        public final String title;
-        public final String content;
-        public final String details;
-        public final String d_day;
-
-        public LifeItem(String id,String title, String content, String details, String d_day) {
-            this.id = id;
-            this.title = title;
-            this.content = content;
-            this.details = details;
-            this.d_day = d_day;
-        }
-
-        @Override
-        public String toString() {
-            return content;
-        }
+    @Override
+    public String toString() {
+        return content;
     }
 }
