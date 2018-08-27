c63ba1cb97f669cf340d616a68e0e88653491ed3
DDONGKKANG <wldudtla123@naver.com>
2018-08-28 00:04:37 +0900

    Merge branch 'master' of C:\Users\user\AndroidStudioProjects\CosmeticLifeCycle with conflicts.

--- app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/CsmtAddActivity.java
+++ app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/CsmtAddActivity.java
@@ -6,6 +6,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.sax.EndElementListener;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -14,6 +16,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,18 +24,36 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TooManyListenersException;
 
 public class CsmtAddActivity extends Activity {
     final int OPEN_DATE = 1;
     final int MAKE_DATE = 2;
     final int END_DATE = 3;
 
+    long mNow;
+    Date mDate;
+    // SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd"); // Log 출력때만 쓰임.
+
+    GregorianCalendar today = new GregorianCalendar();
+
+    private int mDdayOpen = 0;
+    private int mDdayMake = 0;
+    private int mDdayEnd = 0;
+    private int mDdayReal = 0;
+
     private List<String> list = new ArrayList<>();
     private Map<String, CsmtData> csmtMap = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +78,7 @@ public class CsmtAddActivity extends Activity {
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 csmtMap.clear();
-                for(DataSnapshot fileSnapshot : dataSnapshot.getChildren()) {
+                for (DataSnapshot fileSnapshot : dataSnapshot.getChildren()) {
                     CsmtData csmt = fileSnapshot.getValue(CsmtData.class);
                     String tmp = fileSnapshot.child("name").getValue(String.class);
                     System.out.println(tmp);
@@ -80,11 +101,11 @@ public class CsmtAddActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 final String search_csmt = autoCompleteTextView.getText().toString();
-                CsmtData csmt =  csmtMap.get(search_csmt);
+                CsmtData csmt = csmtMap.get(search_csmt);
                 brand.setText(csmt.brand);
                 product.setText(csmt.name);
                 category.setText(csmt.category);
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(autoCompleteTextView.getWindowToken(), 0);
             }
         });
@@ -115,6 +136,7 @@ public class CsmtAddActivity extends Activity {
         open.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 showDialog(OPEN_DATE); // 날짜 설정 다이얼로그 띄우기
+                //   mTextView.setText(getTime());
             }
         });
 
@@ -131,6 +153,187 @@ public class CsmtAddActivity extends Activity {
         });
     }
 
+  /*    private String getTime(){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+
+        mDate.getYear();
+
+        return mFormat.format(mDate);
+    }
+    */
+
+
+    // D-day 계산하는 함수
+    public int countdday(int id, int myear, int mmonth, int mday) {
+        String string = "a";
+        // 제조일 기준, 카테고리에 따라 myear, mmonth 에 넣는 수가 달라짐.
+        switch (string) {
+            case "Skin":
+                myear = myear + 1;  // 제조일로부터 1년
+                break;
+
+            case "Lotion":
+                myear = myear + 1;  // 제조일로부터 1년
+                break;
+
+            case "Cream":
+                myear = myear + 1;  // 제조일로부터 1년
+                break;
+
+            case "Cleansing Form":
+                myear = myear + 1;  // 제조일로부터 1년 6개월
+                mmonth = mmonth + 6;
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Cleansing Water":
+                myear = myear + 1;  // 제조일로부터 1년 6개월
+                mmonth = mmonth + 6;
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Cleansing Oil":
+                myear = myear + 1;  // 제조일로부터 1년 6개월
+                mmonth = mmonth + 6;
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Sunscreen":
+                mmonth = mmonth + 6;  // 제조일로부터 6개월
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Foundation":
+                myear = myear + 1;  // 제조일로부터 1년
+                break;
+
+            case "Cushion":
+                myear = myear + 1;  // 제조일로부터 1년
+                break;
+
+            case "Concealer":
+                myear = myear + 1;  // 제조일로부터 1년
+                break;
+
+            case "Powder":
+                myear = myear + 3;  // 제조일로부터 3년
+                break;
+
+            case "Lipstick":
+                myear = myear + 1;  // 제조일로부터 1년 6개월
+                mmonth = mmonth + 6;
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Lip Tint":
+                myear = myear + 1;  // 제조일로부터 1년 6개월
+                mmonth = mmonth + 6;
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Lip Balm":
+                myear = myear + 1;  // 제조일로부터 1년 6개월
+                mmonth = mmonth + 6;
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Eyeliner":
+                myear = myear + 1;  // 제조일로부터 1년
+                break;
+
+            case "Mascara":
+                mmonth = mmonth + 6;  // 제조일로부터 6개월
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+            case "Shadow":
+                myear = myear + 1;  // 제조일로부터 1년 6개월
+                mmonth = mmonth + 6;
+                if (mmonth >= 12) {
+                    mmonth = mmonth - 12;
+                    myear = myear + 1;
+                }
+                break;
+
+        }
+        switch (id) {
+            case OPEN_DATE:
+                TextView open = (TextView) findViewById(R.id.openDay);
+                open.setText(myear + "년  " + (mmonth + 1) + "월  " + mday + "일");
+                break;
+            case MAKE_DATE:
+                TextView make = (TextView) findViewById(R.id.makeDay);
+                make.setText(myear + "년  " + (mmonth + 1) + "월  " + mday + "일");
+                myear = myear + 3;    // 제조일로부터 3년
+                break;
+            case END_DATE:
+                TextView end = (TextView) findViewById(R.id.endDay);
+                end.setText(myear + "년  " + (mmonth + 1) + "월  " + mday + "일");
+                break;
+        }
+
+        try {
+            Calendar todaCal = Calendar.getInstance(); //오늘날짜 가져오기
+            Calendar ddayCal = Calendar.getInstance(); //오늘날짜를 가져와 변경시킴
+
+            //mmonth -= 1; // 받아온날자에서 -1을 해줘야함.
+            ddayCal.set(myear, mmonth, mday);// D-day의 날짜를 입력
+            Log.e("테스트", mFormat.format(todaCal.getTime()) + "");
+            Log.e("테스트", mFormat.format(ddayCal.getTime()) + "");
+
+            //->(24 * 60 * 60 * 1000) 24시간 60분 60초 * (ms초->초 변환 1000)
+            long today = todaCal.getTimeInMillis() / 86400000;
+            long dday = ddayCal.getTimeInMillis() / 86400000;
+            long count = dday - today; // 오늘 날짜에서 dday 날짜를 빼주게 됩니다.
+
+            return (int) count;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public int pickDday() {
+        if (mDdayMake == 0 && mDdayEnd == 0) {
+            mDdayReal = mDdayOpen;
+        } else if (mDdayEnd == 0) {
+            if (mDdayOpen < mDdayMake)
+                mDdayReal = mDdayOpen;
+            else mDdayReal = mDdayMake;
+        } else {
+            if (mDdayOpen < mDdayEnd)
+                mDdayReal = mDdayOpen;
+            else mDdayReal = mDdayEnd;
+        }
+        Toast.makeText(CsmtAddActivity.this, "D" + mDdayReal, Toast.LENGTH_SHORT).show();
+        return mDdayReal;
+    }
+
     @Override
     @Deprecated
     protected Dialog onCreateDialog(int id) {
@@ -143,13 +346,13 @@ public class CsmtAddActivity extends Activity {
                                 new DatePickerDialog.OnDateSetListener() {
                                     public void onDateSet(DatePicker view,
                                                           int year, int monthOfYear, int dayOfMonth) {
-                                        TextView open = (TextView) findViewById(R.id.openDay);
-                                        open.setText(year + "년  " + (monthOfYear + 1) + "월  " + dayOfMonth + "일");
+                                        mDdayOpen = countdday(OPEN_DATE, year, monthOfYear, dayOfMonth);
+                                        pickDday();
                                     }
                                 }
                                 , // 사용자가 날짜설정 후 다이얼로그 빠져나올때
                                 //    호출할 리스너 등록
-                                2018, 7, 26); // 기본값 연월일, 월은 1달 빼서 넣기
+                                today.get(today.YEAR), today.get(today.MONTH), today.get(today.DAY_OF_MONTH)); // 기본값 연월일, 월은 1달 빼서 넣기
                 return dpd;
 
             case MAKE_DATE:
@@ -158,13 +361,14 @@ public class CsmtAddActivity extends Activity {
                                 new DatePickerDialog.OnDateSetListener() {
                                     public void onDateSet(DatePicker view,
                                                           int year, int monthOfYear, int dayOfMonth) {
-                                        TextView make = (TextView) findViewById(R.id.makeDay);
-                                        make.setText(year + "년  " + (monthOfYear + 1) + "월  " + dayOfMonth + "일");
+
+                                        mDdayMake = countdday(MAKE_DATE, year, monthOfYear, dayOfMonth);
+                                        pickDday();
                                     }
                                 }
                                 , // 사용자가 날짜설정 후 다이얼로그 빠져나올때
                                 //    호출할 리스너 등록
-                                2018, 7, 26); // 기본값 연월일, 월은 1달 빼서 넣기
+                                today.get(today.YEAR), today.get(today.MONTH), today.get(today.DAY_OF_MONTH)); // 기본값 연월일, 월은 1달 빼서 넣기
                 return dpd;
 
             case END_DATE:
@@ -173,16 +377,17 @@ public class CsmtAddActivity extends Activity {
                                 new DatePickerDialog.OnDateSetListener() {
                                     public void onDateSet(DatePicker view,
                                                           int year, int monthOfYear, int dayOfMonth) {
-                                        TextView end = (TextView) findViewById(R.id.endDay);
-                                        end.setText(year + "년  " + (monthOfYear + 1) + "월  " + dayOfMonth + "일");
+
+                                        mDdayEnd = countdday(END_DATE, year, monthOfYear, dayOfMonth);
+                                        pickDday();
                                     }
                                 }
                                 , // 사용자가 날짜설정 후 다이얼로그 빠져나올때
                                 //    호출할 리스너 등록
-                                2018, 7, 26 ); // 기본값 연월일, 월은 1달 빼서 넣기
+                                today.get(today.YEAR), today.get(today.MONTH), today.get(today.DAY_OF_MONTH)); // 기본값 연월일, 월은 1달 빼서 넣기
                 return dpd;
         }
+
         return super.onCreateDialog(id);
     }
-
 }
--- app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/GetInformActivity.java
+++ app/src/main/java/leesimjeonsim/user/cosmeticlifecycle/GetInformActivity.java
@@ -60,20 +60,20 @@ public class GetInformActivity extends Activity implements View.OnClickListener 
         mManButton = findViewById(R.id.join_man);
 
         //SkinTipeButtons
-        mOilyButton = findViewById(R.id.join_skintype1);
-        mDryButton = findViewById(R.id.join_skintype2);
-        mComplexityButton = findViewById(R.id.join_skintype3);
+        mOilyButton = findViewById(R.id.oily_skin);
+        mDryButton = findViewById(R.id.dry_skin);
+        mComplexityButton = findViewById(R.id.complexity_skin);
 
         //SkinInterestButtons
-        mAtopyButton = findViewById(R.id.join_interest1);
-        mAcneButton = findViewById(R.id.join_interest2);
-        mSensitivityButton = findViewById(R.id.join_interest3);
+        mAtopyButton = findViewById(R.id.atopy_interest);
+        mAcneButton = findViewById(R.id.acne_interest);
+        mSensitivityButton = findViewById(R.id.sensitivity_interest);
 
-        mBlushButton = findViewById(R.id.join_interest4);
-        mDeadButton = findViewById(R.id.join_interest5);
-        mPoreButton = findViewById(R.id.join_interest6);
+        mBlushButton = findViewById(R.id.blush_interest);
+        mDeadButton = findViewById(R.id.dead_interest);
+        mPoreButton = findViewById(R.id.pore_interest);
 
-        mElasticityButton = findViewById(R.id.join_interest7);
+        mElasticityButton = findViewById(R.id.elasticity_interest);
 
         mFinishButton = findViewById(R.id.get_finish_button);
 
@@ -116,7 +116,7 @@ public class GetInformActivity extends Activity implements View.OnClickListener 
             mUserdata.setSex(true);
         } else if (i == R.id.join_man) {
             mUserdata.setSex(false);
-        } else if (i == R.id.join_skintype1) {
+        } else if (i == R.id.oily_skin) {
 
             if (((CheckBox) v).isChecked()) {
                 mUserfeature.set_Oily(true);
@@ -128,7 +128,7 @@ public class GetInformActivity extends Activity implements View.OnClickListener 
                 mUserfeature.set_Oily(false);
             }
 
-        } else if (i == R.id.join_skintype2) {
+        } else if (i == R.id.dry_skin) {
 
             if (((CheckBox) v).isChecked()) {
                 mUserfeature.set_Dry(true);
@@ -140,7 +140,7 @@ public class GetInformActivity extends Activity implements View.OnClickListener 
                 mUserfeature.set_Dry(false);
             }
 
-        } else if (i == R.id.join_skintype3) {
+        } else if (i == R.id.complexity_skin) {
 
             if (((CheckBox) v).isChecked()) {
                 mUserfeature.set_Complexity(true);
@@ -152,20 +152,62 @@ public class GetInformActivity extends Activity implements View.OnClickListener 
                 mUserfeature.set_Complexity(false);
             }
 
-        } else if (i == R.id.join_interest1) {
-            mUserfeature.set_Atopy(true);
-        } else if (i == R.id.join_interest2) {
-            mUserfeature.set_Acne(true);
-        } else if (i == R.id.join_interest3) {
-            mUserfeature.set_Sensitivity(true);
-        } else if (i == R.id.join_interest4) {
-            mUserfeature.set_Blush(true);
-        } else if (i == R.id.join_interest5) {
-            mUserfeature.set_Dead(true);
-        } else if (i == R.id.join_interest6) {
-            mUserfeature.set_Pore(true);
-        } else if (i == R.id.join_interest7) {
-            mUserfeature.set_Elasticity(true);
+        } else if (i == R.id.atopy_interest) {
+
+            if (mUserfeature.Atopy == true) {
+                mUserfeature.set_Atopy(false);
+            } else {
+                mUserfeature.set_Atopy(true);
+            }
+
+        } else if (i == R.id.acne_interest) {
+
+            if (mUserfeature.Acne == true) {
+                mUserfeature.set_Acne(false);
+            } else {
+                mUserfeature.set_Acne(true);
+            }
+
+        } else if (i == R.id.sensitivity_interest) {
+
+            if (mUserfeature.Sensitivity == true) {
+                mUserfeature.set_Sensitivity(false);
+            } else {
+                mUserfeature.set_Sensitivity(true);
+            }
+
+        } else if (i == R.id.blush_interest) {
+
+            if (mUserfeature.Blush == true) {
+                mUserfeature.set_Blush(false);
+            } else {
+                mUserfeature.set_Blush(true);
+            }
+
+        } else if (i == R.id.dead_interest) {
+
+            if (mUserfeature.Dead == true) {
+                mUserfeature.set_Dead(false);
+            } else {
+                mUserfeature.set_Dead(true);
+            }
+
+        } else if (i == R.id.pore_interest) {
+
+            if (mUserfeature.Pore == true) {
+                mUserfeature.set_Pore(false);
+            } else {
+                mUserfeature.set_Pore(true);
+            }
+
+        } else if (i == R.id.elasticity_interest) {
+
+            if (mUserfeature.Elasticity == true) {
+                mUserfeature.set_Elasticity(false);
+            } else {
+                mUserfeature.set_Elasticity(true);
+            }
+
         } else if (i == R.id.get_finish_button) {
             setUserdata();
             Intent intent = new Intent(getBaseContext(), MainList_Activity.class);
